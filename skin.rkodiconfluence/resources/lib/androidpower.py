03f053f5e55473b6b70f30ab9ace4f3f34264492
Dregov <dregov@linux.pl>
2017-02-06 07:35:45 +0100

    rKodi

--- skin.rkodiconfluence/resources/lib/androidpower.py
+++ skin.rkodiconfluence/resources/lib/androidpower.py
@@ -5,14 +5,24 @@ def logoff():
 	xbmc.executebuiltin("System.Logoff()")
 	
 def sleep():
-	os.system("su -c 'echo 0 > /sys/devices/virtual/display/display0.HDMI/enable'")
-	os.system("su -c 'read'")
-	os.system("su -c 'echo 1 > /sys/devices/virtual/display/display0.HDMI/enable'")
+	xbmc.executebuiltin("XBMC.ReloadSkin()")
+	xbmc.executebuiltin("XBMC.Quit()")
+	os.system("sleep 2")
+	os.system("su -c 'echo 0 > /sys/class/display/display0.HDMI/enable'")
+	os.system("sleep 5")
+	os.system("su -c 'echo 1 > /sys/class/display/display0.HDMI/enable'")
 	
 def shutdown():
+	xbmc.executebuiltin("XBMC.ReloadSkin()")
+	xbmc.executebuiltin("XBMC.Quit()")
+	os.system("sleep 2")
 	os.system("su -c 'echo 0 > /sys/devices/virtual/display/display0.HDMI/enable'")
-
+	os.system("sleep -1")
+	
 def reboot():
+	xbmc.executebuiltin("XBMC.ReloadSkin()")
+	xbmc.executebuiltin("XBMC.Quit()")
+	os.system("sleep 2")
 	os.system("su -c 'reboot'")
 
 if sys.argv[1] == 'shutdown':
